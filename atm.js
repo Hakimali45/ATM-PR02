e5f9ce1c15755ee761000cee39d6e4be29468fa7
Hakim Ali <161127157+Hakimali45@users.noreply.github.com>
2024-04-02 07:53:03 +0500

    Add files via upload

---
+++ atm.js
@@ -0,0 +1,70 @@
+import inquirer from "inquirer";
+let totalBalance = 10000;
+const pinNumber = 4554;
+let pinEntered = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "Enter your pin number",
+        type: "number",
+    }
+]);
+// console.log(pinEntered.pin)
+if (pinEntered.pin == pinNumber) {
+    let atmQuestions = await inquirer.prompt([
+        {
+            name: "accountType",
+            message: "Enter your account type",
+            type: "list",
+            choices: [
+                "Current Account",
+                "Savings Account",
+            ]
+        },
+        {
+            name: "transMethod",
+            message: "Select your transaction method",
+            type: "list",
+            choices: [
+                "Cash Withdrawal",
+                "Fast Cash"
+            ]
+        }
+    ]);
+    if (atmQuestions.transMethod == "Cash Withdrawal") {
+        let cashwithdrawAmount = await inquirer.prompt([
+            {
+                name: "withdrawal",
+                message: "Enter the amount you want to withdraw",
+                type: "number",
+            }
+        ]);
+        //Greater then or equals to operator
+        if (totalBalance >= cashwithdrawAmount.withdrawal) {
+            totalBalance -= cashwithdrawAmount.withdrawal; // totalBalance = totalBalance - cashwithdrawamount
+            console.log(`Your Total Balance is :${totalBalance}`);
+        }
+        else
+            (console.log(`Insufficient Balance`));
+    }
+    else {
+        let fastCashAmount = await inquirer.prompt([
+            {
+                name: "fastCash",
+                message: "Select the amount you want to withdraw",
+                type: "list",
+                choices: [
+                    "1000",
+                    "2500",
+                    "5000",
+                    "10000"
+                ]
+            }
+        ]);
+        if (totalBalance >= fastCashAmount.fastCash) {
+            totalBalance -= fastCashAmount.fastCash; // totalBalance = totalBalance - cashwithdrawAmount
+            console.log(`Your total balance is :${totalBalance}`);
+        }
+        else
+            (console.log(`Insufficient Balance`));
+    }
+}
